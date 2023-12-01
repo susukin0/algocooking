d403c1703b2e83fb03e9655e6f33672e7a68e79d
susukin0 <70662829+susukin0@users.noreply.github.com>
2023-12-01 18:33:20 +0000

    Add files via upload

---
+++ algocooking1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	int n,answer,sum=0;
+    int N = 100000;
+	int a[N];
+	scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        scanf("%d", &a[i]);
+    }
+	for(int i=0; i<n; i++) {
+       sum += a[i];
+    }
+	int min = a[0];
+	for(int i=0; i<n; i++){
+		if(a[i]<min) min = a[i];
+		}
+	sum -= min;
+	printf("%d", sum);
+    return 0;
+}
+
